2630f0348239fdf67468a74ed7a72150d18453b2
Tim Booher <ads@theboohers.org>
2012-03-30 19:22:50 -0400

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120327012051) do
+ActiveRecord::Schema.define(:version => 20120330232030) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -79,6 +79,13 @@ ActiveRecord::Schema.define(:version => 20120327012051) do
     t.string   "referring_source"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+  end
+
+  create_table "services", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "users", :force => true do |t|
