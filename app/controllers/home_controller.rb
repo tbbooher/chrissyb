fb307bf542a9d19ad9b7656fd536750605cc1aa1
Tim Booher <ads@theboohers.org>
2012-03-30 20:08:52 -0400

    updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,14 @@ class HomeController < ApplicationController
   def index
     @users = User.all
   end
+
+  def services
+     @services = Service.all
+  end
+  def bio
+
+  end
+  def payment
+
+  end
 end
